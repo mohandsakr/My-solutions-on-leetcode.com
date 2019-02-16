05bc566d0af8837b77de1240f6805592f34ce6d2
mohand reda sakr <mohandsakr69@gmail.com>
2019-02-17 00:18:57 +0200

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,26 @@
+
+//problem link:https://leetcode.com/problems/add-two-numbers/
+//problem name:Two Sum
+//status:accept 
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+vector<int> vect;
+        bool found=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(nums[i]+nums[j]==target&&!found&&i!=j){
+                    vect.push_back(i);
+                    vect.push_back(j);
+                    found=1;
+                    break;
+                   
+                }
+            }
+        }
+        
+         return  vect;
+    }
+};
