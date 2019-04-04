3bec9f7c2701c176e175d3c9b6d6e0c0531eb0f2
mohand reda sakr <mohandsakr69@gmail.com>
2019-04-04 05:48:30 +0200

    Create 28. Implement strStr().cpp

---
+++ 28. Implement strStr().cpp
@@ -0,0 +1,25 @@
+
+
+
+/*
+ problem link:https://leetcode.com/problems/implement-strstr/
+ problem name:28. Implement strStr().
+ status:accpted
+ author :mohand sakr
+ 
+ 
+
+
+*/
+
+
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        return haystack.find(needle);
+        
+    }
+};
