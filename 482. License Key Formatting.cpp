16ee8ad05c6309e398e0764406998002a685d891
mohand reda sakr <mohandsakr69@gmail.com>
2019-04-10 01:14:52 +0200

    Create 482. License Key Formatting.cpp

---
+++ 482. License Key Formatting.cpp
@@ -0,0 +1,46 @@
+/*
+ problem link:https://leetcode.com/problems/license-key-formatting/
+ problem name:482. License Key Formatting
+ status:accpted
+ author :mohand sakr
+*/
+
+class Solution {
+public:
+     string licenseKeyFormatting(string s, int k) {
+        string s2,s1;
+        int len=s.length();
+        bool first=0;
+        
+        int charnum=0; 
+        for(int i=0;i<len;i++){
+            if(s[i]!='-')s1+=toupper(s[i]);
+        
+            
+        }
+        
+                int len2=s1.length();
+        int mod=len2%k;
+    int count=0;
+
+        for(int i=0;i<len2;i++){
+            if(!first &&count==mod&&mod){
+                first=1;
+                s2+='-';
+                ++count;
+            }
+            else if(i&&(abs(i-mod)%k==0)){
+                s2+='-';
+            }
+            
+            
+            s2+=s1[i];
+             
+        }
+        
+        
+        return s2;
+        
+    }
+};
+
