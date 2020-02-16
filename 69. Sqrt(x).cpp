ba0e2e8996b29293fa6910a69df539a1b031a5c8
mohand reda sakr <mohandsakr69@gmail.com>
2020-02-16 16:39:37 +0200

    Create 69. Sqrt(x).cpp

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,16 @@
+
+
+/*
+problem link:https://leetcode.com/problems/sqrtx/
+problem name: 69. Sqrt(x) ;
+auhtor : mohand sakr ;
+status: accepted ;
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        return (int )sqrt(x);
+    }
+};
